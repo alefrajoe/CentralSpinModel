d6070a6b4f3dd5aba7211a285038157331983476
alessio <alessiofranchi4@gmail.com>
2023-01-27 17:59:45 +0100

    analysis file complete, build_file has been added

--- analysis_measurement.py
+++ analysis_measurement.py
@@ -7,6 +7,9 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
+START_DIR = "C:/Users/Utente/Desktop/central_spin_model"
+os.chdir(START_DIR)
+
 # define the directory where all data are stored
 DIR = 'data_centralspin'
 
@@ -25,9 +28,12 @@ for file in FILES:
         # transform data to dataframe
         df = pd.DataFrame(data, columns=obs)
         # define a column containing the decoherence factor
-        df["deco"] = 0.5 * (1 - df["magx"]**2 + df["magy"]**2 + df["magz"]**2)
+        df["deco"] = 0.5 * (1 - (df["magx"]**2 + df["magy"]**2 + df["magz"]**2))
         # group quantities by their step value -> all quantities with the same value of step are listed together. We finally compute the mean values of all values grouped
         df_grouped = df.groupby("step").mean()
         # plot observables and averaged observables
-        plt.plot(df["time"], df["deco"])
-        plt.plot(df_grouped["time"], df_grouped["deco"])
+        #plt.plot(df["time"]/int(df["L"][0]), df["magy"])
+        plt.plot(df_grouped["time"]/int(df["L"][0]), df_grouped["magy"], label="L="+str(int(df["L"][0])))
+        
+plt.legend()
+plt.show()
